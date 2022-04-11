5e491312f4bcaedf638a38fdd8f58311a98fd0cd
Alex Sweeney <a.sweeney@ryte.com>
2022-04-11 15:06:55 +0100

    added React.memo example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Todos from './Todos.jsx';
 
-function App() {
+// click button = new state = new render
+// memo inside Todos stops it rerendering unless props updated
+
+// add todo = todos changed
+// todos = prop of Todos component = Todos component re-renders
+
+function App() { 
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+  const [inputValue, setInputValue] = useState('');
+
+  const increment = () => {
+    setCount(c => c + 1);
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  };
+
+  const handleClickTodoAdd = (e) => {
+    e.preventDefault();
+     
+    setTodos(todos => [...todos, inputValue])
+    setInputValue('')
+  };
+  console.log('todos', todos)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <Todos todos={todos} />
+    <hr />
+
+    <form>
+      <input type="text" value={inputValue} onChange={handleInputChange} name="todo"></input>
+      <button onClick={handleClickTodoAdd}>Add Todo</button>
+    </form>
+
+    <div>
+      Count: {count}
+      <button onClick={increment}>+</button>
     </div>
+   </>
   );
-}
+} 
 
 export default App;
